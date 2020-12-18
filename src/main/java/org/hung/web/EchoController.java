2ce7c923f3dc55a63041e4d45660a87beb5e2d9f
Kwong-Hung YIP <kwonghung.yip@gmail.com>
2020-12-18 17:12:53 +0800

    Update grafana dashboard

--- src/main/java/org/hung/web/EchoController.java
+++ src/main/java/org/hung/web/EchoController.java
@@ -2,9 +2,14 @@ package org.hung.web;
 
 import java.security.Principal;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 public class EchoController {
 
@@ -16,5 +21,25 @@ public class EchoController {
 	@GetMapping("/greeting")
 	public String greeting(Principal princpal) {
 		return "Hello " + princpal.getName();
+	}
+	
+	@GetMapping("/chance")
+	public ResponseEntity<String> chance() {
+		if (Math.random() > 0.3) {
+			return ResponseEntity.ok("success");
+		} else {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("error");
+		}
+	}
+	
+	@GetMapping("/delay")
+	public String delay() {
+		long delay = Math.round(Math.random()*1000*3);
+		try {
+			Thread.currentThread().sleep(delay);
+		} catch (InterruptedException e) {
+			log.info("wake up");
+		}
+		return "After " + delay + " ms delay.";
 	}
 }
